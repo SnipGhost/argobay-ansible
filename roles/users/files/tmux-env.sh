845baaf73ef3a539eb40ccb2e4e322aa2d87efbe
Mikhail Kucherenko <makucherenko@list.ru>
2021-02-23 04:11:50 +0300

    Add itsuki & rikka to tmux-env

--- roles/users/files/tmux-env.sh
+++ roles/users/files/tmux-env.sh
@@ -6,9 +6,9 @@ tmux new-session -s 'cluster' \
 	new-window -n 'nino' "ssh $name@nino" \; \
 	new-window -n 'miku' "ssh $name@miku" \; \
 	new-window -n 'yotsuba' "ssh $name@yotsuba" \; \
+	new-window -n 'itsuki' "ssh $name@itsuki" \; \
+	new-window -n 'rikka' "ssh $name@rikka" \; \
 	select-window -t 0 \;
 
-	# new-window -n 'itsuki' "ssh $name@itsuki" \; \
-	# new-window -n 'rikka' "ssh $name@rikka" \; \
 	# new-window -n 'metroid' "ssh $name@metroid" \; \
 	# new-window -n 'yukinon' "ssh $name@yukinon" \; \

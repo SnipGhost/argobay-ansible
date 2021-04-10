cf2fafbdfaf191dd05087434ee9e172f86e5c647
Mikhail Kucherenko <makucherenko@list.ru>
2021-04-10 19:27:42 +0300

    Fix first mass-install bugs

--- roles/users/files/tmux-env.sh
+++ roles/users/files/tmux-env.sh
@@ -2,6 +2,7 @@
 name=$(whoami)
 tmux new-session -s 'cluster' \
 			   -n 'asuna' "ssh $name@asuna" \; \
+	new-window -n 'zelda' "ssh $name@zelda" \; \
 	new-window -n 'ichika' "ssh $name@ichika" \; \
 	new-window -n 'nino' "ssh $name@nino" \; \
 	new-window -n 'miku' "ssh $name@miku" \; \

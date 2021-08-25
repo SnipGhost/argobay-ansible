fde71df49bcbdcf33d68cdff409d55835cddb7c6
Mikhail Kucherenko <makucherenko@list.ru>
2021-08-26 02:37:44 +0300

    Fix tmux-env and .my.cnf for admins

--- roles/users/files/tmux-env.sh
+++ roles/users/files/tmux-env.sh
@@ -2,13 +2,13 @@
 name=$(whoami)
 tmux new-session -s 'cluster' \
 			   -n 'asuna' "ssh $name@asuna" \; \
-	new-window -n 'zelda' "ssh $name@zelda" \; \
 	new-window -n 'ichika' "ssh $name@ichika" \; \
 	new-window -n 'nino' "ssh $name@nino" \; \
 	new-window -n 'miku' "ssh $name@miku" \; \
 	new-window -n 'yotsuba' "ssh $name@yotsuba" \; \
 	new-window -n 'itsuki' "ssh $name@itsuki" \; \
 	new-window -n 'rikka' "ssh $name@rikka" \; \
+	new-window -n 'zelda' "ssh $name@zelda" \; \
 	select-window -t 0 \;
 
 	# new-window -n 'metroid' "ssh $name@metroid" \; \

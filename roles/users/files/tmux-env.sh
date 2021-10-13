11c5fa586b4bfa908485ceff3f9026210ba1c53b
Mikhail Kucherenko <makucherenko@list.ru>
2021-10-13 23:22:35 +0300

    Update tmux-env script

--- roles/users/files/tmux-env.sh
+++ roles/users/files/tmux-env.sh
@@ -1,15 +1,81 @@
 #!/bin/sh
-name=$(whoami)
-tmux new-session -s 'cluster' \
-			   -n 'asuna' "ssh $name@asuna" \; \
-	new-window -n 'ichika' "ssh $name@ichika" \; \
-	new-window -n 'nino' "ssh $name@nino" \; \
-	new-window -n 'miku' "ssh $name@miku" \; \
-	new-window -n 'yotsuba' "ssh $name@yotsuba" \; \
-	new-window -n 'itsuki' "ssh $name@itsuki" \; \
-	new-window -n 'rikka' "ssh $name@rikka" \; \
-	new-window -n 'zelda' "ssh $name@zelda" \; \
-	select-window -t 0 \;
 
-	# new-window -n 'metroid' "ssh $name@metroid" \; \
-	# new-window -n 'yukinon' "ssh $name@yukinon" \; \
+usage() {
+	echo "Usage: $0 [-s <session_name>] [-n <username>] [-i <hosts>] [-f] [-h]" 1>&2
+	exit 1
+}
+
+kill_session() {
+	echo "Kill session '$session'"
+	tmux kill-session -t $session
+}
+
+create_session() {
+	echo "Create new session '$session'"
+	first=$(echo $inventory | tr -s ' ' | cut -f1 -d\ )
+	tmux new-session -d -s $session -n "$first" "ssh $name@$first"
+	for host in $(echo $inventory | tr -s ' ' | cut -f2- -d\ ); do
+		tmux new-window -t ${session}: -n "$host" "ssh $name@$host"
+	done
+	tmux select-window -t ${session}:0
+}
+
+# Parse arguments
+while getopts "s:n:i:fh" o; do
+	case "${o}" in
+		s)
+			session="${OPTARG}"
+			;;
+		n)
+			name="${OPTARG}"
+			;;
+		i)
+			inventory="${OPTARG}"
+			unique_inventory=1
+			;;
+		f)
+			force_create=1
+			;;
+		h)
+			usage
+			;;
+		*)
+			usage
+			;;
+	esac
+done
+
+# Default values
+if [ -z "$session" ]; then
+	session="cluster"
+fi
+if [ -z "$name" ]; then
+	name=$(whoami)
+fi
+if [ -z "$inventory" ]; then
+	inventory="asuna ichika nino miku yotsuba itsuki rikka metroid zelda rapunzel mai yukinon"
+fi
+
+# Check if the session is already exists
+tmux has-session -t $session 2>/dev/null
+has_session_rc=$?
+
+# Kill session if flag "force" is passed
+if [ $has_session_rc -eq 0 ] && ! [ -z $force_create ]; then
+	kill_session
+	has_session_rc=1
+fi
+
+# Set up new session if not exists
+if [ $has_session_rc != 0 ]; then
+	create_session
+	new_session_created=1
+fi
+
+# Print warning for inventory argument
+if [ -z "$new_session_created" ] && ! [ -z "$unique_inventory" ]; then
+	echo "The session has already running, the 'inventory' argument will be ignored!"
+fi
+
+# And attach to session
+tmux attach-session -t $session
